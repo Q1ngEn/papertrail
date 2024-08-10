5b77296494fb17ea39f2cee5fb93f42de2b1866e
Q1ngEn <158569639+Q1ngEn@users.noreply.github.com>
2024-08-10 21:57:30 +0800

    Add files via upload

---
+++ papertrail.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, url_for, redirect, request, flash
+from forms import RegistrationForm, LoginForm
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'e52751703579269d27d78a5b0957e6a6'
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == "admin@blog.com" and form.password.data == "password":
+            flash("You have been logged in!", "success")
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template("login.html", title="Login", form=form)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f"Account created for {form.username.data}!", "success")
+        return redirect(url_for('home'))
+    return render_template("register.html", title="Register", form=form)
+
+if __name__ == '__main__':
+    app.run(debug=True)
