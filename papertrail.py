7378b6ed65f6423b90a432f76a6e44aa3d0d339c
Q1ngEn <158569639+Q1ngEn@users.noreply.github.com>
2024-08-11 21:21:59 +0800

    Added backend for deposit

--- papertrail.py
+++ papertrail.py
@@ -36,6 +36,18 @@ def login():
     return render_template("login.html", title="Login", form=form)
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == "admin@blog.com" and form.password.data == "password":
+            flash("You have been logged in!", "success")
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template("login.html", title="Login", form=form)
+
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
