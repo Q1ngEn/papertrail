dfd8120b3fb555e16646655bd07b6b07bf08956c
Q1ngEn <158569639+Q1ngEn@users.noreply.github.com>
2024-08-16 20:57:44 +0800

    added sqlalchemy
    it allows the app to access a database

--- papertrail/__init__.py
+++ papertrail/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'e52751703579269d27d78a5b0957e6a6'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
 
 from papertrail import routes
