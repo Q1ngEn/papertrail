6f883b6b0249d02a29906a30a9e05e66af03653f
Q1ngEn <158569639+Q1ngEn@users.noreply.github.com>
2024-08-17 22:21:02 +0800

    Update __init__.py

--- papertrail/__init__.py
+++ papertrail/__init__.py
@@ -5,11 +5,14 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'e52751703579269d27d78a5b0957e6a6'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///papertrail.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'  # style of message
+
+with app.app_context():
+    db.create_all()
 
 from papertrail import routes
